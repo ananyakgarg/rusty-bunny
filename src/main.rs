7798efac0924d6aefc77604fc729b7e2066ae60c
Ananya Garg <ananyag@ad.unc.edu>
2024-08-28 21:35:19 -0400

    set up base repo and core search functionality

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/search?<cmd>")]
+fn search(cmd: String) -> &'static str {
+    println!("You typed in: {}", cmd);
+    "Hello from search page!"
+}
+
+fn main() {
+    rocket::ignite()
+    .mount("/", routes![index, search])
+    .launch();
+}
